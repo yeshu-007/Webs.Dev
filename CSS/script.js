8bb016c9cace0e05a7e93ee91e5186dc9b8b88a3
yeshu-007 <yeshwanthchettyr2006@gmail.com>
2025-10-17 16:22:13 +0530

    challenge1 js

--- CSS/script.js
+++ CSS/script.js
@@ -1,31 +1,23 @@
-// let oldcost=document.getElementById("listprice")
-// var newcost=document.getElementById("saleprice")
-
-// var discountedPrice=oldcost.textContent -(oldcost.textContent*5)/100
-// console.log(discountedPrice)
-// newcost.textContent= discountedPrice
 
 
-var buttonElement = document.getElementById("apply-discount");
+var discbuttonElement = document.getElementById("apply-discount");
 
-buttonElement.addEventListener("click", function() {
-
+discbuttonElement.addEventListener("click", function() {
+ 
     let oldcostElement = document.getElementById("listprice");
     let oldcost = oldcostElement.textContent;
-    console.log(oldcost);
 
     let discountvalue = document.getElementById("discount").valueAsNumber;
-    console.log(discountvalue);
 
     let newcostElement = document.getElementById("saleprice");
-    console.log(newcostElement);
-
+    
     let salepriceValue = oldcost.replace("₹", "");
-    console.log(salepriceValue);
 
     let finalPrice = parseFloat(salepriceValue) - (parseFloat(salepriceValue) * discountvalue) / 100;
-    console.log(finalPrice);
 
     newcostElement.textContent = "₹" + finalPrice;
 
 });
+
+
+
